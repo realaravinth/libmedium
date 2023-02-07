5e0c0ee62d9d9e3f528820cc62c7e55aae45bf83
realaravinth <realaravinth@batsense.net>
2023-02-07 06:36:54 +0000

    Deploying to gh-libmedium from  @ 4cbd8caf35b06cbd712d4147ab7752a40a8edf7c 🚀

--- implementors/core/default/trait.Default.js
+++ implementors/core/default/trait.Default.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"libmedium":[["impl&lt;'a, 'b&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.65.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"libmedium/post/struct.PositionMap.html\" title=\"struct libmedium::post::PositionMap\">PositionMap</a>&lt;'a, 'b&gt;"]]
+"libmedium":[["impl&lt;'a, 'b&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"libmedium/post/struct.PositionMap.html\" title=\"struct libmedium::post::PositionMap\">PositionMap</a>&lt;'a, 'b&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
