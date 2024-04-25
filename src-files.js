ff8fdb1a0ea976b10e435faabb7a7552f894247f
realaravinth <realaravinth@batsense.net>
2024-04-25 10:36:56 +0000

    Deploying to gh-libmedium from  @ d2dcd6b048b16a11ec517738764fdf9666f8757a 🚀

--- src-files.js
+++ src-files.js
@@ -1,4 +1,4 @@
-var srcIndex = JSON.parse('{\
-"libmedium":["",[],["data.rs","main.rs","meta.rs","post.rs","proxy.rs","render_html.rs","routes.rs","settings.rs"]]\
-}');
+var srcIndex = new Map(JSON.parse('[\
+["libmedium",["",[],["data.rs","main.rs","meta.rs","post.rs","proxy.rs","render_html.rs","routes.rs","settings.rs"]]]\
+]'));
 createSrcSidebar();
---
+++ trait.impl/core/default/trait.Default.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"libmedium":[["impl&lt;'a, 'b&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.77.2/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"libmedium/post/struct.PositionMap.html\" title=\"struct libmedium::post::PositionMap\">PositionMap</a>&lt;'a, 'b&gt;"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.77.2/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"libmedium/post/struct.ListState.html\" title=\"struct libmedium::post::ListState\">ListState</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
